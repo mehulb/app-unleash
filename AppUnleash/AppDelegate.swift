14d560137e13dda15548d24cd26d3fbc073162cd
mehulb <b.mehul.p@gmail.com>
2021-01-18 10:12:48 +0530

    Created macOS proj

---
+++ AppUnleash/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  AppUnleash
+//
+//  Created by Mehul Bhavani on 18/01/21.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ AppUnleash/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  AppUnleash
+//
+//  Created by Mehul Bhavani on 18/01/21.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
